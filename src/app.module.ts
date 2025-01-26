8bdc5d38813fd8606f41b74a4dad90db6c01f05c
Noam Bezalel <1noambez@gmail.com>
2025-01-26 17:52:51 +0200

    adding dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING)],
   controllers: [AppController],
   providers: [AppService],
 })
