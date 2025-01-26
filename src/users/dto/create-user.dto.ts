1ace97ec9a3dcc45484644ae7151398a8e0eb784
Noam Bezalel <1noambez@gmail.com>
2025-01-26 21:04:05 +0200

    adding features for dtos

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,6 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  readonly username: string;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly password: string;
+}
